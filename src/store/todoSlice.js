88cb573b996b941c81a5d81733702d8255097178
ValeryKot <kot.valery@gmail.com>
2022-12-23 12:29:28 +0300

    after store

---
+++ src/store/todoSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const todoSlice = createSlice({
+  name: 'todos',
+  initialState: {
+    todos: [],
+  },
+  reducers: {
+    addTodo(state, action) {
+      console.log(state);
+      console.log(action);
+      state.todos.push({
+        id: new Date().toISOString(),
+        text: action.payload.text,
+        completed: false,
+      });
+    },
+    removeTodo(state, action) {},
+    toggleTodoCompleted(state, action) {},
+  },
+});
+
+export const { addTodo, removeTodo, toggleTodoCompleted } = todoSlice.actions;
+export default todoSlice.reducer;
